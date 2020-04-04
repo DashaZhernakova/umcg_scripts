256a271abb48226a4c075c1e443c7f785fb62954
DashaZhernakova <dashazhernakova@gmail.com>
2020-04-04 15:44:50 +0300

    script for gearshift added

--- gender_differences/preprocessing_gam_fitting_functions.R
+++ gender_differences/preprocessing_gam_fitting_functions.R
@@ -142,46 +142,6 @@ plot_scatter_and_gam2 <- function(merged_tab, pheno_name, correctForCellCounts, 
   }
   
   
-  ylims <- with(merged_tab, range(phenotype))
-  ## draw base plot
-  palette(c(col2transparent("indianred1", 125),col2transparent("dodgerblue1", 125)))
-  #p1 <- plot(phenotype ~ age, data = merged_tab,  col = gender_F1M2,  pch = 16, 
-  #     main = paste0(pheno_name, ' ', label, "\nGAM interaction p = ", format(m_o_p, digits = 3)), 
-  #     cex = 0.6, xlab = "age", ylab = pheno_name)
-  
-  #p1 <- ggplot(merged_tab, aes(y = phenotype, x = age,  col = gender_F1M2)) + geom_point(aes(col = gender_F1M2)) + 
-  #  scale_color_manual(values = c(col2transparent("indianred1", 125),col2transparent("dodgerblue1", 125),"indianred1", "dodgerblue1")) + 
-  #  theme(legend.position = "none", plot.title = element_text(hjust = 0.5), 
-  #        axis.line.x = element_line(colour = 'grey', size=0.5, linetype='solid'),
-  #        axis.line.y = element_line(colour = 'grey', size=0.5, linetype='solid')) + 
-  #  ylab(pheno_name) + 
-  #  ggtitle(paste0(pheno_name, ' ', label, "\nGAM interaction p = ", format(m_o_p, digits = 3))) + 
-  #  scale_x_discrete(name ="age", limits=seq(10,80,10))
-  
-  #levs <- levels(merged_tab$gender_F1M2)
-  #cols = c("indianred1", "dodgerblue1")
-  
-  ## add the fitted lines
-  #for (l in seq_along(levs)) {
-  #  dd <- pdat[pdat$gender_F1M2 == levs[l],]
-  #  #lines(pred ~ age, data = dd, col = cols[[l]], lwd = 2)
-  #  p1 <- p1 + geom_line(data = dd, aes(x= age, y = pred), colour = cols[[l]], size = 1)
-  #}
-  
-  #Plot the difference
-  #if (! is.null(res_dif = NULL)){
-  #p2 <- plot(res_dif$age, res_dif$diff, type = 'l')
-  #p2 <- ggplot(res_dif, aes(x = age, y = diff)) + geom_line(size = 0.8) + 
-  #  theme(legend.position = "none", plot.title = element_text(hjust = 0.5), 
-  #        axis.line.x = element_line(colour = 'grey', size=0.2, linetype='solid'),
-  #        axis.line.y = element_line(colour = 'grey', size=0.2, linetype='solid')) + 
-  #  ylab("Difference men vs women")
-  
-  #plot with conf interval
-  #ggplot(res_dif, aes(x = age, y = diff)) +
-  #  geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2) +
-  #  geom_line()
-  #}
   if (make_plots){
     cex_main = 1
     ylims <- with(merged_tab, range(phenotype))
@@ -338,3 +298,129 @@ plot_scatter_and_gamlss <- function(merged_tab, pheno_name, n_points = 300, make
   }
   
 }
+
+# Fit a GAM with age : gender interaction and (optional) correction for cell counts
+ggplot_scatter_and_gam2 <- function(merged_tab, pheno_name, correctForCellCounts, n_points = 300, make_plots, label = '', gam_family = gaussian()){
+  colnames(merged_tab)[1] <- "phenotype"
+  merged_tab <- merged_tab[(merged_tab$age < 75) & (merged_tab$age >= 20),]
+  
+  merged_tab <- mutate(merged_tab, ord_gender_F1M2 = ordered(gender_F1M2, levels = c('1', '2')))
+  merged_tab <- mutate(merged_tab, gender_F1M2 = factor(gender_F1M2))
+  #pheno_name = gene_table[gene_table[,1] == colnames(merged_tab)[1],2]
+  
+  women <- merged_tab$gender_F1M2 == 1
+  res_dif = NULL
+  
+  if (!correctForCellCounts){
+    
+    m1 <- gam(phenotype ~ gender_F1M2 + s(age) + s(age, by = gender_F1M2), data = merged_tab, method = "REML", family = gam_family)
+    m_o <- gam(phenotype ~ ord_gender_F1M2 + s(age) + s(age, by = ord_gender_F1M2), 
+               data = merged_tab, method = 'REML', family = gam_family)
+    
+    
+    m_o_p <- summary(m_o)$s.pv[length(summary(m_o)$s.pv)]
+    m_o_g_beta <- m_o$coefficients["ord_gender_F1M2.L"]
+    m_o_g_pv <- summary(m_o)$p.pv["ord_gender_F1M2.L"]
+    
+    if (m_o_p > 0.05){
+      return (list("dif" = NULL, "inter_p" = m_o_p,"g_beta" = m_o_g_beta, "g_pv" = m_o_g_pv))
+    }
+    pdat <- with(merged_tab, expand.grid(age = seq(20, 75, length = n_points), 
+                                         gender_F1M2 = c('1', '2')))
+    pdat <- transform(pdat, pred = predict(m1, newdata = pdat, type = "response"))
+    
+    
+    
+    if (! "Ordered Categorical" %in% gam_family$family){
+      pdat_dif <- expand.grid(age = seq(20, 75, length = n_points),
+                              gender_F1M2 = c('1', '2'))
+      #res_dif <- smooth_diff(m1, pdat_dif, '2', '1', "gender_F1M2")
+      #res_dif$age = seq(20, 75, length = n_points)
+      res_dif <- simple_diff(pdat)
+    } else { # stupid way to go
+      m1 <- gam(phenotype ~ gender_F1M2 + s(age) + s(age, by = gender_F1M2), data = merged_tab, method = "REML")
+      pdat <- with(merged_tab, expand.grid(age = seq(20, 75, length = n_points), 
+                                           gender_F1M2 = c('1', '2')))
+      pdat <- transform(pdat, pred = predict(m1, newdata = pdat, type = "response"))
+      
+    }
+  } else { # Correct for cell counts
+    
+    m1 <- gam(phenotype ~ gender_F1M2 + ba + eo + er + gr + 
+                ly + mo + tr + s(age, by = gender_F1M2), data = merged_tab, method = "REML")
+    
+    m_o <- gam(phenotype ~ ord_gender_F1M2 + s(age) + ba + eo + er + gr + 
+                 ly + mo + tr + s(age, by = ord_gender_F1M2), 
+               data = merged_tab, method = 'REML')
+    m_o_p <- summary(m_o)$s.pv[length(summary(m_o)$s.pv)]
+    m_o_g_beta <- m_o$coefficients["ord_gender_F1M2.L"]
+    m_o_g_pv <- summary(m_o)$p.pv["ord_gender_F1M2.L"]
+    
+    if (m_o_p > 0.05){
+      return (list("dif" = NULL, "inter_p" = m_o_p,"g_beta" = m_o_g_beta, "g_pv" = m_o_g_pv))
+    }
+    
+    pdat <- with(merged_tab, expand.grid(age = seq(20, 75, length = n_points), gender_F1M2 = c('1', '2'), 
+                                         ba = mean(ba), eo = mean(eo), er = mean(er), gr = mean(gr), 
+                                         ly = mean(ly),  mo = mean(mo), tr = mean(tr)))
+    pdat <- transform(pdat, pred = predict(m1, newdata = pdat, type = "response"))
+    
+    if (! "Ordered Categorical" %in% gam_family$family){
+      pdat_dif <- expand.grid(age = seq(20, 75, length = n_points), gender_F1M2 = c('1', '2'), 
+                              ba = mean(merged_tab$ba), eo = mean(merged_tab$eo), er = mean(merged_tab$er), gr = mean(merged_tab$gr), 
+                              ly = mean(merged_tab$ly),  mo = mean(merged_tab$mo), tr = mean(merged_tab$tr))
+      #res_dif <- smooth_diff(m1, pdat_dif, '2', '1', "gender_F1M2")
+      #res_dif$age = seq(20, 75, length = n_points)
+      res_dif <- simple_diff(pdat)
+    } else { # stupid way to go
+      m1 <- gam(phenotype ~ gender_F1M2 + s(age) + s(age, by = gender_F1M2), data = merged_tab, method = "REML")
+      pdat <- with(merged_tab, expand.grid(age = seq(20, 75, length = n_points), 
+                                           gender_F1M2 = c('1', '2')))
+      pdat <- transform(pdat, pred = predict(m1, newdata = pdat, type = "response"))
+    }
+  }
+  
+  
+  ylims <- with(merged_tab, range(phenotype))
+  ## draw base plot
+  palette(c(col2transparent("indianred1", 125),col2transparent("dodgerblue1", 125)))
+  
+  p1 <- ggplot(merged_tab, aes(y = phenotype, x = age,  col = gender_F1M2)) + geom_point(aes(col = gender_F1M2)) + 
+    scale_color_manual(values = c(col2transparent("indianred1", 125),col2transparent("dodgerblue1", 125),"indianred1", "dodgerblue1")) + 
+    theme(legend.position = "none", plot.title = element_text(hjust = 0.5), 
+          axis.line.x = element_line(colour = 'grey', size=0.5, linetype='solid'),
+          axis.line.y = element_line(colour = 'grey', size=0.5, linetype='solid')) + 
+    ylab(pheno_name) + 
+    ggtitle(paste0(pheno_name, ' ', label, "\nGAM interaction p = ", format(m_o_p, digits = 3))) + 
+    scale_x_discrete(name ="age", limits=seq(10,80,10))
+  
+  levs <- levels(merged_tab$gender_F1M2)
+  cols = c("indianred1", "dodgerblue1")
+  
+  ## add the fitted lines
+  for (l in seq_along(levs)) {
+    dd <- pdat[pdat$gender_F1M2 == levs[l],]
+    p1 <- p1 + geom_line(data = dd, aes(x= age, y = pred), colour = cols[[l]], size = 1)
+  }
+  
+  #Plot the difference
+  if (! is.null(res_dif = NULL)){
+
+  p2 <- ggplot(res_dif, aes(x = age, y = diff)) + geom_line(size = 0.8) + 
+    theme(legend.position = "none", plot.title = element_text(hjust = 0.5), 
+          axis.line.x = element_line(colour = 'grey', size=0.2, linetype='solid'),
+          axis.line.y = element_line(colour = 'grey', size=0.2, linetype='solid')) + 
+    ylab("Difference men vs women")
+  
+  #plot with conf interval
+  #ggplot(res_dif, aes(x = age, y = diff)) +
+  #  geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2) +
+  #  geom_line()
+  }
+  if (make_plots){
+    print(p1)
+    print(p2)
+  }
+  
+  return (list("pdat" = pdat, "dif" = res_dif$diff, "inter_p" = m_o_p,"g_beta" = m_o_g_beta, "g_pv" = m_o_g_pv))
+} 
---
+++ gender_differences/run_gearshift.R
@@ -0,0 +1,90 @@
+
+source("/groups/umcg-lifelines/tmp01/users/umcg-dzhernakova/scripts/umcg_scripts/gender_differences/preprocessing_gam_fitting_functions.R")
+
+
+wd_path <- "/groups/umcg-lifelines/tmp01/users/umcg-dzhernakova/gender_difs/"
+
+# Phenotypes
+traits_path <- "Laboratory_assessment_Blood_1A.dat"
+st_col = 2
+
+plot_basepath <- paste0("plots/", traits_path, ".pdf")
+
+pheno_path <- "age_gender_all_LL.txt"
+
+correct_for_cellcounts = F
+make_plots = T
+
+setwd(wd_path)
+
+# traits of interest
+traits0 <- read.delim(traits_path, header = T, row.names = 1, sep = "\t", as.is = T, check.names = F)
+traits0 <- traits0[,seq(st_col,ncol(traits0))]
+traits <- sapply(traits0, function(x) as.numeric(as.character(x)))
+row.names(traits) <- row.names(traits0)
+
+# Age, gender and other phenotypes
+pheno0 <- read.delim(pheno_path, header = T, row.names = 1, sep = "\t", as.is = T, check.names = F)
+pheno <- na.omit(pheno0)
+
+traits_m <- traits[match(row.names(pheno), row.names(traits), nomatch = 0 ),]
+pheno_m <- pheno[match(row.names(traits_m), row.names(pheno), nomatch = 0),]
+all(row.names(traits_m) == row.names(pheno_m))
+num_traits <- ncol(traits_m)
+num_traits
+
+traits_m <- traits_m[order(pheno_m$age),]
+pheno_m <- pheno_m[order(pheno_m$age),]
+
+nplotspp = 20
+n_points = 300
+res_dif_all <- data.frame(age = seq(20, 75, length = n_points))
+res_summary <- data.frame()
+
+cnt = 1
+if (make_plots){
+  pdf(plot_basepath, width = 15, height = 15)
+  par(mfrow=c(5,4)) 
+}
+
+
+indices = 1:ncol(traits_m)
+cnt = 1
+
+for (idx in indices){
+  if (cnt > nplotspp & make_plots){
+    cnt = 1
+    par(mfrow=c(5,4))
+  }
+  print(idx)
+  
+  trait_id <- colnames(traits_m)[idx]
+  trait_name = trait_id
+  if (length(trait_name) > 0 & length(unique(traits_m[,idx])) > 1){ #if gene id in gene convertion table
+    merged_tab <- rm_na_outliers(traits_m, pheno_m, idx, method = "IQR", log_tr = F)
+    #merged_tab[,1] = merged_tab[,1] + 1
+    res_dif = NULL
+    #tryCatch({
+    
+    res_dif_lst <- plot_scatter_and_gam2(merged_tab, trait_name, correctForCellCounts = F, n_points = 300, make_plots = make_plots, gam_family = gaussian(), label = '')
+    #},error=function(e) {
+    #      message(paste("Fitting failed for ", idx))
+    # })
+    
+    #if (res_dif_lst[["inter_p"]] < 0.05){
+    #  cnt <- cnt + 1
+    #  res_dif_all[,trait_id] <- res_dif_lst[["dif"]]
+    #}
+    res_summary[trait_id,'inter_p'] = res_dif_lst[["inter_p"]]
+    res_summary[trait_id,'g_beta'] = res_dif_lst[["g_beta"]]
+    res_summary[trait_id,'g_pv'] = res_dif_lst[["g_pv"]]
+  }
+  
+}
+write.table(res_summary, file = paste0("summary_tables/", traits_path, ".txt"), sep = "\t", quote = F, col.names = NA)
+nrow(res_summary[res_summary$inter_p < 0.05,])
+nrow(res_summary[res_summary$g_p < 0.05 | res_summary$inter_p < 0.05,])
+
+if (make_plots){
+  dev.off()
+}
