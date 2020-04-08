50336d72f48f8ea758d37a8fd9311476e43447b2
DashaZhernakova <dashazhernakova@gmail.com>
2020-04-08 15:41:16 +0300

    small update

--- gender_differences/run_microbiome_boxy.R
+++ gender_differences/run_microbiome_boxy.R
@@ -1,10 +1,11 @@
 # Microbiome
-traits_path <- "LLD.metaphlan.raw.tsv"
+source("/groups/umcg-lld/tmp03/umcg-dzhernakova/umcg_scripts/gender_differences/preprocessing_gam_fitting_functions.R")
+traits_path <- "/groups/umcg-lifelines/tmp03/projects/dag3_fecal_mgs/DAG3_data_ready/microbiome/processed/DAG3_metaphlan_bacteria_archaea_nofiltering.txt"
 st_col = 1
 wd_path = "/groups/umcg-lld/tmp03/umcg-dzhernakova/gender_difs/microbiome/"
 plot_basepath <- "DAG3_microbiome_gamlss_poly.pdf"
 
-pheno_path <- "age_gender_smk_contrac_cell_counts.cc_outliers_na.txt"
+pheno_path <- "dag3_age_gender.txt"
 
 correct_for_cellcounts = F
 make_plots = T
@@ -12,13 +13,13 @@ make_plots = T
 setwd(wd_path)
 
 # traits of interest
-traits0 <- as.data.frame(t(read.table(traits_path, header = T, row.names = 1, sep = "\t", as.is = T, check.names = F)))
+traits0 <- read.delim(traits_path, header = T, row.names = "ID", sep = "\t", as.is = T, check.names = F)
 traits0 <- traits0[,seq(st_col,ncol(traits0))]
 traits <- sapply(traits0, function(x) as.numeric(as.character(x)))
 row.names(traits) <- row.names(traits0)
 
 # Age, gender and other phenotypes
-pheno0 <- as.data.frame(t(read.table(pheno_path, header = T, row.names = 1, sep = "\t", as.is = T, check.names = F)))
+pheno0 <- read.delim(pheno_path, header = T, row.names = 1, sep = "\t", as.is = T, check.names = F)
 pheno <- na.omit(pheno0)
 traits_m <- traits[match(row.names(pheno), row.names(traits), nomatch = 0 ),]
 
@@ -33,7 +34,7 @@ pheno_m$gm_mean <- gm_mean_matrix(traits_m)
 # Remove bacteria with < 100 individuals
 traits_m <- traits_m[ ,colSums(traits_m > 0) >= 100]
 # convert to fraction
-traits_m <- traits_m/100
+#traits_m <- traits_m/100
 # make smaller than one
 traits_m[traits_m == 1] <- 0.9999999
 
@@ -80,47 +81,47 @@ for (idx in indices){
   res_models[[idx]] = inter_pval
   #cnt = cnt + 1
   if (inter_pval < 0.05){
-  pdat <- with(merged_tab, expand.grid(age = seq(20, 75, length = n_points), 
-                                       gender_F1M2 = c('1', '2'), gm_mean = mean(gm_mean)))
-  pdat <- transform(pdat, pred = predict(m1, newdata = pdat, type = "response"))
-  pdat_dif <- expand.grid(age = seq(20, 75, length = n_points),
-                          gender_F1M2 = c('1', '2'))
-  res_dif <- simple_diff(pdat)
-  
-  ylims = c(min(0, merged_tab[,1], pdat$pred), max(merged_tab[,1], pdat$pred))
-  
-  
-  ylabel <- pheno_name
-  if (nchar(pheno_name) > 40){
-    spl <- unlist(strsplit(pheno_name, "\\|"))
-    ylabel <- spl[length(spl)]
-    cex_main <- 0.8
-    if (nchar(pheno_name) > 50) cex_main <- 0.7
-    if (nchar(pheno_name) > 60) cex_main <- 0.6
+    pdat <- with(merged_tab, expand.grid(age = seq(20, 75, length = n_points), 
+                                         gender_F1M2 = c('1', '2'), gm_mean = mean(gm_mean)))
+    pdat <- transform(pdat, pred = predict(m1, newdata = pdat, type = "response"))
+    pdat_dif <- expand.grid(age = seq(20, 75, length = n_points),
+                            gender_F1M2 = c('1', '2'))
+    res_dif <- simple_diff(pdat)
     
-  }
-  
-  
-  ## draw base plot
-  palette(c(col2transparent("indianred1", 125),col2transparent("dodgerblue1", 125)))
-  plot(phenotype ~ age, data = merged_tab,  col = gender_F1M2,  pch = 16, 
-       main = paste0(pheno_name, ' ', label, "\nLR.test interaction p = ", format(inter_pval, digits = 3)), 
-       cex = 0.6, xlab = "age", ylab = ylabel, cex.main = cex_main, ylim = ylims)
-  
-  levs <- levels(merged_tab$gender_F1M2)
-  cols = c("indianred1", "dodgerblue1")
-  
-  ## add the fitted lines
-  for (l in seq_along(levs)) {
-    dd <- pdat[pdat$gender_F1M2 == levs[l],]
-    lines(pred ~ age, data = dd, col = cols[[l]], lwd = 2)
+    ylims = c(min(0, merged_tab[,1], pdat$pred), max(merged_tab[,1], pdat$pred))
     
+    
+    ylabel <- pheno_name
+    if (nchar(pheno_name) > 40){
+      spl <- unlist(strsplit(pheno_name, "\\|"))
+      ylabel <- spl[length(spl)]
+      cex_main <- 0.8
+      if (nchar(pheno_name) > 50) cex_main <- 0.7
+      if (nchar(pheno_name) > 60) cex_main <- 0.6
+      
+    }
+    
+    
+    ## draw base plot
+    palette(c(col2transparent("indianred1", 125),col2transparent("dodgerblue1", 125)))
+    plot(phenotype ~ age, data = merged_tab,  col = gender_F1M2,  pch = 16, 
+         main = paste0(pheno_name, ' ', label, "\nLR.test interaction p = ", format(inter_pval, digits = 3)), 
+         cex = 0.6, xlab = "age", ylab = ylabel, cex.main = cex_main, ylim = ylims)
+    
+    levs <- levels(merged_tab$gender_F1M2)
+    cols = c("indianred1", "dodgerblue1")
+    
+    ## add the fitted lines
+    for (l in seq_along(levs)) {
+      dd <- pdat[pdat$gender_F1M2 == levs[l],]
+      lines(pred ~ age, data = dd, col = cols[[l]], lwd = 2)
+      
+    }
+    
+    #Plot the difference
+    plot(res_dif$age, res_dif$diff, type = 'l')
   }
-  
-  #Plot the difference
-  plot(res_dif$age, res_dif$diff, type = 'l')
-  }
-  }
+}
 
 dev.off()
 
