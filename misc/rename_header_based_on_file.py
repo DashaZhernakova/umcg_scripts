2645d9032ab5a2573721652f9e457d973d82a193
DashaZhernakova <dashazhernakova@gmail.com>
2021-11-02 13:25:51 +0300

    read from stdin

--- misc/rename_header_based_on_file.py
+++ misc/rename_header_based_on_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/local/python
 import sys
+import gzip
 
 in_fname = sys.argv[1]
 rename_fname = sys.argv[2]
@@ -9,21 +10,30 @@ if len(sys.argv) > 3:
     col_old = sys.argv[3]
     col_new = sys.argv[4]
 
+if in_fname != "stdin":
+    if in_fname.endswith(".gz"):
+        in_file = gzip.open(in_fname)
+    else:
+        in_file = open(in_fname)
+else:
+    in_file = sys.stdin
+
+
 rename_dict = {}
 with open (rename_fname) as f:
     for l in f:
         spl = l.rstrip().split("\t")
         rename_dict[spl[col_old]] = spl[col_new]
 
-with open(in_fname) as f:
-    header = f.readline().rstrip().split("\t")
-    for h in header:
-        if len(h) > 0:
-            h2 = rename_dict[h]
-        else:
-            h2 = h
-        out_str += "\t" + h2
-    print(out_str.replace("\t", "", 1))
 
-    for l in f:
-        print(l.rstrip())
+header = in_file.readline().rstrip().split("\t")
+for h in header:
+    if len(h) > 0:
+        h2 = rename_dict[h]
+    else:
+        h2 = h
+    out_str += "\t" + h2
+print(out_str.replace("\t", "", 1))
+
+for l in in_file:
+    print(l.rstrip())
