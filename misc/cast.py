a2876c38b8b3aec60ae8179d602a1b4ec8788d78
DashaZhernakova <dashazhernakova@gmail.com>
2022-01-20 19:54:46 +0300

    added second column to collate

--- misc/cast.py
+++ misc/cast.py
@@ -11,11 +11,22 @@ else:
 col_key = int(sys.argv[2])
 col_val = int(sys.argv[3])
 
+col_val2 = None
+if (len(sys.argv) > 4):
+    col_val2 = int(sys.argv[3])
+    dict2 = defaultdict(set)
+
 map_dict = defaultdict(set)
 for line in f:
-	spl = line.rstrip().split("\t")
-	map_dict[spl[col_key]].add(spl[col_val])
+    spl = line.rstrip().split("\t")
+    map_dict[spl[col_key]].add(spl[col_val])
+    if col_val2 is not None:
+        dict2[spl[col_key]].add(spl[col_val2])
 
 
 for k, v in map_dict.items():
-    print (k + "\t" + ",".join(v))
+    if col_val2 is None:
+        print (k + "\t" + ",".join(v))
+    else:
+        second = dict2.get(k, "NA")
+        print (k + "\t" + ",".join(v) + "\t" + ",".join(second))
