636dc9683c6b8b8dbdc3091c39bff1c134ee784f
DashaZhernakova <dashazhernakova@gmail.com>
2023-12-22 19:16:06 +0300

    added file

--- misc/rename_table_values.py
+++ misc/rename_table_values.py
@@ -7,7 +7,7 @@ conversion_fname = sys.argv[3]
 key_col = int(sys.argv[4])
 value_col = int(sys.argv[5])
 
-data = pd.read_csv(fname, sep = "\t") 
+data = pd.read_csv(fname, sep = "\t", header = None) 
 sample_conversion = pd.read_csv(conversion_fname, sep = "\t") 
 dict_conversion = dict(zip(sample_conversion.iloc[:,key_col], sample_conversion.iloc[:,value_col]))
 
@@ -16,4 +16,4 @@ for col in map(int,cols.split(',')):
     data.iloc[:,col] = [dict_conversion.get(id, id) for id in id_list]
 
 
-data.to_csv(sys.stdout, sep="\t", na_rep="NA", index = False)
+data.to_csv(sys.stdout, sep="\t", na_rep="NA", header = False, index = False)
